e06833437533f6457db496d611bc4f8512fba517
atshaw1994 <atshaw1994@gmail.com>
2025-08-05 20:04:14 -0400

    Save/Load feature

--- hangman.rb
+++ hangman.rb
@@ -1,9 +1,30 @@
+require 'yaml'
+
 @dictionary = File.readlines('dict.txt').map(&:chomp)
-@secret_word = "" # ex. "cat"
-@board = [] # ex. ["_", "_", "_"] for "cat"
 @incorrect_guesses = []
 
-def initialize_game
+def init_game
+  puts "Welcome to Hangman!"
+  puts "Do you want to (1) start a new game or (2) load a saved game?"
+  print "Enter 1 or 2: "
+  choice = gets.chomp
+
+  case choice
+  when "1"
+    init_variables()
+  when "2"
+    unless load_game()
+      puts "Starting a new game instead."
+      init_variables()
+    end
+  else
+    puts "Invalid choice. Starting a new game."
+    init_variables()
+  end
+  
+end
+
+def init_variables
   valid_words = @dictionary.select { |word| word.length.between?(5, 12) }
   @secret_word = valid_words.sample
   if @secret_word.nil?
@@ -106,16 +127,62 @@ def check_win
   return !@board.include?("_")
 end
 
+def save_game
+  game_state = {
+    secret_word: @secret_word,
+    board: @board,
+    incorrect_guesses: @incorrect_guesses
+  }
+  print "Enter a name for your save file: "
+  filename = gets.chomp + ".yaml"
+  File.open(filename, 'w') do |file|
+    file.write(YAML.dump(game_state))
+  end
+  puts "Game saved as '#{filename}'!"
+end
+
+def load_game
+  saved_games = Dir.glob('*.yaml')
+  if saved_games.empty?
+    puts "No saved games found."
+    return false
+  end
+
+  puts "Select a saved game to load:"
+  saved_games.each_with_index { |file, index| puts "#{index + 1}. #{file}" }
+  print "Enter the number of the file: "
+  file_number = gets.chomp.to_i - 1
+
+  if saved_games[file_number]
+    filename = saved_games[file_number]
+    game_state = YAML.load(File.read(filename))
+    @secret_word = game_state[:secret_word]
+    @board = game_state[:board]
+    @incorrect_guesses = game_state[:incorrect_guesses]
+    puts "Game loaded from '#{filename}'!"
+    return true
+  else
+    puts "Invalid selection."
+    return false
+  end
+end
+
+# Main Game Loop
 userin_playgame = "Y"
 while userin_playgame.downcase == "y"
-  initialize_game()
+  init_game()
   while @incorrect_guesses.length < 7 && !check_win()
     print_image
     print_board()
-    puts "Incorrect: " + @incorrect_guesses.join(", ")
-    print "Enter letter: "
-    user_guess = gets.chomp
-    check_letter(user_guess)
+    print "Enter letter or 'save' to save game: "
+    user_input = gets.chomp.downcase
+
+    if user_input == "save"
+      save_game()
+      break
+    else
+      check_letter(user_input)
+    end
   end
 
   if check_win()
